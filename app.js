ccae77ffdc422dc7f1b17aeb9ef2b5f69dc0f28f
tansen2425 <dinislamtansen11@gmail.com>
2023-10-14 14:34:41 +0600

    done

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let inputqr= document.getElementById("inputqr");
 let imgqr= document.getElementById("imgqr");
-let boxb=document.getElementsByClassName("box");
+let boxB=document.getElementById("box");
 const getqr =()=>{
 imgqr.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+inputqr.value;
-
+inputqr.value="";
+boxB.style.width= "384px";
+boxB.style.height="384px";
 
 }
 btnqr.addEventListener("click",getqr);
